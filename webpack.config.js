80036699dffe4c79f154aa516c6a5f794539bbf2
ggalushko <titanmelchior@gmail.com>
2023-04-13 12:40:20 +0300

    improve css processing

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: { main: "./src/scripts/main.js" },
+  entry: {
+    main: "./src/scripts/main.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
@@ -13,9 +15,9 @@ module.exports = {
   mode: "development",
   devServer: {
     static: path.resolve(__dirname, "./dist"),
+    open: true,
     compress: true,
     port: 8080,
-    open: true,
   },
   module: {
     rules: [
@@ -25,17 +27,21 @@ module.exports = {
         exclude: "/node_modules/",
       },
       {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
           },
+          "postcss-loader",
         ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
       },
     ],
   },
